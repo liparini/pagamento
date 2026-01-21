57f483abf3956ad1f8f04848e23cec3a76023af4
Carol <43304498+liparini@users.noreply.github.com>
2026-01-20 23:21:06 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,19 +1,25 @@
-import { auth } from "./firebase.js";
+import { app } from "./firebase.js";
 import {
+  getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-btnGoogle.onclick = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider);
-};
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
+const btnLogin = document.getElementById("btnLogin");
+const btnLogout = document.getElementById("btnLogout");
+
+btnLogin.onclick = () => signInWithPopup(auth, provider);
 btnLogout.onclick = () => signOut(auth);
 
-onAuthStateChanged(auth, user => {
-  loginBox.style.display = user ? "none" : "block";
-  app.style.display = user ? "block" : "none";
+onAuthStateChanged(auth, (user) => {
+  document.getElementById("cadastro").style.display = user ? "block" : "none";
+  document.getElementById("lista").style.display = user ? "block" : "none";
+  document.getElementById("dashboard").style.display = user ? "block" : "none";
+  btnLogin.style.display = user ? "none" : "inline";
+  btnLogout.style.display = user ? "inline" : "none";
 });
